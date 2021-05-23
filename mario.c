a98641e455a69bafb9a5ca0b2eddcd49f65cb742
fon318 <elfonsec@ucsc.edu>
2021-05-22 23:47:03 -0700

    Inverse Mario pyramid

--- mario.c
+++ mario.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void){
-   int height;
-do{
-   height = get_int("Enter a height between 1 and 8:");
-}
-while(height<1||height>8);
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Enter a height between 1 and 8:");
+    } while (height < 1 || height > 8);
 
+    for (int i = 0; i < height; i++)
+    {
+        
+        for (int j = 0; j < height; j++)
+        {
+          //side note: if dont remember how to do this later, don't sweat it
+          //try to solve it first as a 4x4 , 3x3, 5x5 square
+          //"" "" "" * <= remember that take into account "" as characters that will be looping as well
+            if (i + j < height - 1)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("*");
+            }
+        }
 
-for(int i=0; i<height;i++){
-
-   for(int j=0; j<height;j++){
-
-    //side note: if you don't get this later , don't sweat it
-    //try first to solve is as a regular 8x8, 4x4, 3x4 etc. square
-    //"" "" "" *  <= then solve it as if "" were characters
-     if(i+j<height-1){
-        printf(" ");
-     }
-     else{
-        printf("*");
-     }
-   }
-
-printf("\n");
-}
-
+        printf("\n");
+    }
 }
